2607c9d43c5f6f68258cbb7a2e483db6de5887c3
shivam <shivam201483101@vcet.edu.in>
2024-03-30 17:31:27 +0530

    soluu

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Ensure pip is installed
-# python3.9 -m ensurepip
+python3.9 -m ensurepip
 
-# python3.9 -m pip install --upgrade pip
+python3.9 -m pip install --upgrade pip
 
-# python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 #python3.9 manage.py collectstatic --noinput --clear
