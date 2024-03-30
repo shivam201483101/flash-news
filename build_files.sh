b114560730ecb779d663499f02cbadcdbb538550
shivam <shivam201483101@vcet.edu.in>
2024-03-30 16:55:52 +0530

    sol

--- build_files.sh
+++ build_files.sh
@@ -5,4 +5,4 @@ python3.9 -m ensurepip
 python3.9 -m pip install --upgrade pip
 
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+#python3.9 manage.py collectstatic --noinput --clear
