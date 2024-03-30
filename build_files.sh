ed155799bd41cfcfb58586012211be740eed61cd
shivam <shivam201483101@vcet.edu.in>
2024-03-30 16:18:10 +0530

    first

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Ensure pip is installed
-python3.9 -m ensurepip
+#python3.9 -m ensurepip
 
-# Upgrade pip to the latest version
-python3.9 -m pip install --upgrade pip
 
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
